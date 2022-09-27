2b0c34e12bbfad0b1db80d2c523d6bf18f553a53
SahruzRiyad <sahruzzaman01@gmail.com>
2022-09-27 21:55:17 +0600

    First commit

---
+++ In Odd Even Rank Print/Odd_and_Even_Print.c
@@ -0,0 +1,21 @@
+// -----------MPI----------
+//To compile-  mpicc file_name.c -o file_name
+//To run-  mpiexec -n number_of_process ./file_name
+//To avoid warning-  mpiexec -quiet -n number_of_process ./file_name
+
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc , char ** argv){
+    int my_rank , com_sz;
+
+    MPI_Init(NULL, NULL);
+    MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
+    MPI_Comm_size(MPI_COMM_WORLD,&com_sz);
+
+    if(my_rank & 1)
+        printf("World from Rank: %d\n",my_rank);
+    else 
+        printf("Hello from Rank: %d\n",my_rank);
+    return MPI_Finalize();
+}
